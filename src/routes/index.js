18121dc580b6fb23480804dd290f9a3a703a8cf9
Guillaume Pitot <gpitot@nine.com.au>
2019-08-21 16:51:04 +1000

    vw route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
+
 import express from 'express';
+import vw from './projects/6825_vw';
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('this is the api home')
 });
+
+router.use('/vw', vw);
 
 export default router;
---
+++ src/routes/projects/6825_vw/index.js
@@ -0,0 +1,73 @@
+
+import express from 'express';
+
+var router = express.Router();
+
+
+
+//check code word given against code word of day
+const codes = [
+    {
+        date : 'August 21 2019',
+        code : 'TEST'
+    },
+    {
+        date : 'September 08 2019',
+        code : 'ALL STARS'
+    },
+    {
+        date : 'September 09 2019',
+        code : 'ENSUITE'
+    },
+    {
+        date : 'September 10 2019',
+        code : 'TERRACE'
+    },
+    {
+        date : 'September 11 2019',
+        code : 'CHALLENGE'
+    },
+    {
+        date : 'September 12 2019',
+        code : 'TOOLS DOWN'
+    }
+]
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    const userCode = req.body.code;
+    if (!userCode) {
+        res.status(200);
+        res.json({success:false});
+    } 
+
+    const currentDate = new Date();
+
+    for (let i=0; i<codes.length; i+=1) {
+        const {date, code} = codes[i];
+        const dateCheck = new Date(date);
+        if (
+            currentDate.getDate() ===  dateCheck.getDate()
+            &&
+            currentDate.getMonth() ===  dateCheck.getMonth()
+            &&
+            currentDate.getYear() ===  dateCheck.getYear()
+        ) {
+            if (code.toLowerCase() === userCode.toLowerCase()) {
+                res.status(200);
+                return res.json({success: true});
+            } else {
+                res.status(200);
+                return res.json({success: false});
+            }
+        }
+    }
+
+    res.status(200);
+    return res.json({success: false});
+    
+
+});
+
+
+export default router;
