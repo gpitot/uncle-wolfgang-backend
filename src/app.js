46966767eacdd30ef7fe6804831e6f52924e0cb4
Guillaume Pitot <gpitot@nine.com.au>
2019-08-27 12:04:52 +1000

    cors and remove static files

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import AWS from 'aws-sdk';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
+
+AWS.config.region = process.env.REGION;
 
 const app = express();
 
@@ -11,7 +15,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname, '../public')));
+app.options(/\.nine\.com\.au$/, cors())
+
+app.get('/', (req, res) => {
+    res.send('powered api');
+})
 
 app.use('/api', indexRouter);
 
