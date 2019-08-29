650ad2dcc5d17db0798bce75a381fe5cfcbc428f
Guillaume Pitot <gpitot@nine.com.au>
2019-08-29 17:08:26 +1000

    instagram login page, import instagram

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,25 @@
 
 import express from 'express';
 import vw from './projects/6825_vw';
+import instagram from './projects/instagram-source';
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('this is the api home')
+  res.send(`
+    <h3>list of routes</h3>
+
+    <ul>
+    <li>/vw</li>
+    <li>/instagram</li>
+    </ul>
+  `)
 });
 
 router.use('/vw', vw);
+router.use('/instagram', instagram);
+
+
 
 export default router;
