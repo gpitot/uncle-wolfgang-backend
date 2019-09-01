842da3b1e941c1b37efe8e2146198c7b2d3f9cf7
Guillaume Pitot <gpitot@nine.com.au>
2019-09-02 09:57:23 +1000

    package ebex

--- gulpfile.js
+++ gulpfile.js
@@ -11,23 +11,25 @@ const VERSION = argv.VERSION;
 
 gulp.task('build', function() {
     //copy package.json to build folder
-    return gulp.src('package.json')
+    return gulp.src([
+        'package.json',
+        '.ebextensions'
+        ])
         .pipe(gulp.dest('build/'));
 })
 
-gulp.task('ebexte', function() {
-    //copy package.json to build folder
-    return gulp.src('.ebextensions')
-        .pipe(gulp.dest('build/'));
-})
 
 gulp.task('package', function() {
     //zipped folder of build
-    return gulp.src('build/**')
+    return gulp.src([
+        'build/**'
+        ], {
+            dot : true
+        })
         .pipe(zip(`${PROJECT}-${VERSION}.zip`))
         .pipe(gulp.dest('temp/'));
 });
 
-gulp.task('default', gulp.series('build', 'ebexte', 'package'), function(cb) {
+gulp.task('default', gulp.series('build', 'package'), function(cb) {
     cb();
 });
