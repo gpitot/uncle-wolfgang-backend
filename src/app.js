f099f774d7ad7e0285fb49f042929e29e3a5c8a6
Guillaume Pitot <gpitot@nine.com.au>
2019-09-02 14:39:48 +1000

    remove cors and public dir

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.options(/\.nine\.com\.au$/, cors())
+//app.use(express.static(path.join(__dirname, '../public')));
+//app.options(/\.nine\.com\.au$/, cors())
 
 app.get('/', (req, res) => {
     res.send('powered api');
