223ba364141b1a1cdf96612a8582313f6bad3c82
Guillaume Pitot <gpitot@nine.com.au>
2019-09-03 16:32:44 +1000

    enable cors other way

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, '../public')));
 //app.options(/\.nine\.com\.au$/, cors())
-app.options('*', cors());
+app.use(cors())
 
 
 app.get('/', (req, res) => {
