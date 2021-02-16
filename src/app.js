3c0f634fb62578d9a114d639c4528acb2193991a
Guillaume Pitot <gpitot@atlassian.com>
2021-02-16 15:09:58 +1100

    all roads lead to rome

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "/frontend")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/index.html"));
 });
 
