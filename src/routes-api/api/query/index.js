825057e974b47c89cfc348ecdf32ceed2166b6cc
Guillaume Pitot <gpitot@atlassian.com>
2021-02-24 10:54:47 +1100

    query update

--- src/routes-api/api/query/index.js
+++ src/routes-api/api/query/index.js
@@ -33,19 +33,45 @@ router.get("/", async (req, res) => {
 
   /*
 
-    insert into ladder_ranks
+   insert into ladder_ranks
  (player_id, ladder_id, rank)
- values ('106578756968220466564', 1, 5000)
-
+ values ('101573359282867079048', 1, 8750)
  */
-  const t = `
-  select * from events;
+  //   const t = `
+  //   insert into ladders (name, description) values ('North Manly Youth League', 'The best young players in the region.')
+  //     `;
+  //   //unique_player_ladder_ranks
+  //   const s = `ALTER TABLE ladder_ranks
+  //   DROP CONSTRAINT unique_player_ladder_ranks;
+  //   `;
 
-  ;
-    `;
+  //   let a = `
+  //     insert into events (name, description, spots, start, open) values
+  //     ('Monday coaching', 'Monday Coaching. $10 for an hour with world class coaches', 8, '2021-03-01 19:00:00', '2021-02-27 12:00:00'),
+  //     ('Monday session 1', 'Monday Social from 7 to 830pm. $17.50 available to all skill levels.', 9, '2021-03-01 19:00:00', '2021-02-27 12:00:00'),
+  //     ('Monday session 2', 'Monday Social from 830 to 10pm. $17.50 available to all skill levels.', 9, '2021-03-01 19:00:00', '2021-02-27 12:00:00')
+  //   `;
+
+  // let initialStart = new Date("2021-03-01 19:00:00");
+  // let initialOpen = new Date("2021-02-27 12:00:00");
+
+  // const format = (d) => {
+  //   return `${d.getFullYear()}-`
+  // }
+
+  // for (let i = 0; i < 50; i += 1) {
+  //   a += ` ('Monday session 1', 'Monday Social from 7 to 830pm. $17.50 available to all skill levels.', 9, '${initialStart}', '${initialOpen}' )`;
+  //   initialStart.setDate(initialStart.getDate() + 7);
+  //   initialOpen.setDate(initialOpen.getDate() + 7);
+  // }
+  // console.log(a);
+
+  const b = `
+  select * from events;
+  `;
 
   //2020-12-19T08:16:04.714Z"
-  query(t)
+  query(b)
     .then((result) => {
       res.send({
         result: result.rows,
