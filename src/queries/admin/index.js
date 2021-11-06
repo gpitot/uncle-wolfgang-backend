216fb8c9d3a22faca38db213bdf1c5d2cc72c3a0
gpitot <gpitot@atlassian.com>
2021-11-06 13:10:46 +1100

    findPotentialSocialSignups

--- src/queries/admin/index.js
+++ src/queries/admin/index.js
@@ -64,9 +64,50 @@ const getRecentlyCreatedUsers = async () => {
   });
 };
 
+// should always recalculate streaks before calling this
+const getUsersWithFreebieSession = async () => {
+  await recalculateAllStreaks();
+  const sql = `select id, firstname, lastname, phone from users 
+    where streak = 2 or streak = 5 or streak = 8 or streak = 11`;
+  return new Promise((resolve, reject) => {
+    query(sql)
+      .then((data) => {
+        resolve(data.rows);
+      })
+      .catch(reject);
+  });
+};
+
+// gets a list of people who could be potential signups
+// based on freebie streaks
+// and on recent signups
+const findPotentialSocialSignups = async () => {
+  const people = [];
+
+  try {
+    people.push(await getUsersWithFreebieSession());
+  } catch (e) {
+    return [];
+  }
+
+  try {
+    const recentlyCreated = await getRecentlyCreatedUsers();
+    recentlyCreated.forEach((recent) => {
+      if (!people.find((person) => person.id === recent.id)) {
+        people.push(recent);
+      }
+    });
+  } catch (e) {
+    return [];
+  }
+
+  return people;
+};
+
 export {
   usersWithMostSocialSignups,
   usersWithNoSocialSignups,
   recalculateAllStreaks,
   getRecentlyCreatedUsers,
+  findPotentialSocialSignups,
 };
