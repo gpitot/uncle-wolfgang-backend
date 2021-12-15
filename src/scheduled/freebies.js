a5701b74f50fbcb1113b9119e8e1d4a05bc29011
gpitot <gpitot@atlassian.com>
2021-12-15 15:37:27 +1100

    checking heroku time

--- src/scheduled/freebies.js
+++ src/scheduled/freebies.js
@@ -3,7 +3,7 @@ import { getUsersWithFreebieSession } from "../queries/admin";
 
 const sendFreebieMessages = async () => {
   const date = new Date();
-  console.log("date.getDay() ", date.getDay());
+  console.log("date.getDay() ", date.getDay(), date.getHours());
   if (date.getDay() === 1) {
     const users = await getUsersWithFreebieSession();
     await sendGroupMessage(users, "social-freebie-reminder");
